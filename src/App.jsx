743b219efbc35c92040b59e3955b79fd5881b106
Kushyar <koshyar2000@gmail.com>
2025-10-24 18:57:44 +0330

    Showcase Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar"
 import ProductViewer from "./components/ProductViewer"
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from "gsap/all"
+import ShowCase from "./components/ShowCase"
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -12,6 +13,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <ProductViewer />
+            <ShowCase />
         </main>
     )
 }
